8e80806b2e4d7ceff541b1e9f71f6ce31375c8bf
likorsi <43271724+likorsi@users.noreply.github.com>
2018-10-23 09:14:26 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,5 +1,5 @@
 def encrypt_caesar(plaintext):
-    """
+    " " "
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -8,19 +8,19 @@ def encrypt_caesar(plaintext):
     'Sbwkrq3.6'
     >>> encrypt_caesar("")
     ''
-   """
+   " " "
     ciphertext=""
     for i in plaintext:
-        if 'A'<=i<='W' or 'a'<=i<='w':
-            ciphertext+=chr(ord(i)+3)
-        elif 'X'<=i<='Z' or 'x'<=i<='z':
-            ciphertext+=chr(ord(i)-23)
+        if 'A' <= i <= 'W' or 'a' <= i <='w':
+            ciphertext+=chr(ord(i) + 3)
+        elif 'X' <= i <= 'Z' or 'x' <= i <= 'z':
+            ciphertext+=chr(ord(i) - 23)
         elif '!' <= i <= '@':
             ciphertext += i
     return ciphertext
 
 def decrypt_caesar(ciphertext):
-    """
+    " " "
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -28,13 +28,13 @@ def decrypt_caesar(ciphertext):
     >>> decrypt_caesar("Sbwkrq3.6")
     'Python3.6'
     >>> decrypt_caesar("")
-    """
+    " " "
     plaintext = ""
     for i in ciphertext:
         if 'D' <= i <= 'Z' or 'd' <= i <= 'z':
             plaintext += chr(ord(i) - 3)
         elif 'A' <= i <= 'C' or 'a' <= i <= 'c':
             plaintext += chr(ord(i) + 23)
-        elif '!'<= i <='@':
+        elif '!' <= i <= '@':
             plaintext += i
     return plaintext
