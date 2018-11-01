649714f6f5aab7ae9a880eccd1d07d241f4ecfd7
likorsi <43271724+likorsi@users.noreply.github.com>
2018-11-01 19:15:37 +0300

    Update vigener.py

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -7,45 +7,21 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    d = {} #  словарь (d-dictionary) для заглавных букв алфавита
-    v=0 # v - value, номер буквы в алфавите
-    #  заполним ключи и значения словаря буквами и их номером в алфавите соответственно:
-    for i in range(ord('A'), ord('Z')):
-        d[chr(i)] = v
-        v+=1
-    d_small = {} #  словарь для строчных букв алфавита
-    v=0
-    #  ананлогичное заполнение словаря, но строчными буквами:
-    for i in range(ord('a'), ord('z')):
-        d_small[chr(i)] = v
-        v+=1
     ciphertext = ""
-    #  продублируем ключ, пока его длина не превысит шифруемое слово:
     keyword *= len(plaintext) // len(keyword) + 1
-    key = [] # список для номера в алфавите букв ключа
-    letters = [] #  список для номера в алфавите букв шифруемого слова
-    #  присвоим элементам порядковый номер буквы в алфавите с помощью ключа словаря:
-    for i in keyword:
-        if 'A'<=i<='Z':
-            key.append(d[i])
+    for i in range(len(plaintext)):
+        key = ord(keyword[i])
+        if 'A' <= plaintext[i] <= 'Z':
+            key -= ord('A')
         else:
-            key.append(d_small[i])
-    for i in plaintext:
-        if 'A'<=i<='Z':
-            letters.append(d[i])
-        else:
-            letters.append(d_small[i])
-    #  найдем верную букву по ключу словаря и добавим ее в зашифрованное слово:
-    for i in range(len(letters)):
-        s = (letters[i]+key[i]) % 26
-        if 'A'<=plaintext[1]<='Z':
-            for k, v in d.items():  # k-key; v-value
-                if s == v:
-                    ciphertext += k
-        else:
-            for k, v in d_small.items():  # k-key; v-value
-                if s == v:
-                    ciphertext += k
+            key -= ord('a')
+        sum = (ord(plaintext[i]) + key)
+        if sum > ord('Z') and 'A' <= plaintext[i] <= 'Z':
+            sum -= 26
+        elif sum > ord('z') and 'a' <= plaintext[i] <= 'z':
+            sum -= 26
+        ciphertext += chr(sum)
+    print(ciphertext)
     return ciphertext
 
 
@@ -58,40 +34,19 @@ def decrypt_vigenere(ciphertext, keyword):
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    #  значения словарей и списков и действия с ними аналогичны функции encrypt_vigenere
-    d = {}
-    v=0 # v - value
-    for i in range(ord('A'), ord('Z')):
-        d[chr(i)] = v
-        v+=1
-    d_small = {}
-    v=0
-    for i in range(ord('a'), ord('z')):
-        d_small[chr(i)] = v
-        v+=1
     plaintext = ""
     keyword *= len(ciphertext) // len(keyword) + 1
-    key = []
-    letters = []
-    for i in keyword:
-        if 'A'<=i<='Z':
-            key.append(d[i])
+    for i in range(len(ciphertext)):
+        key = ord(keyword[i])
+        if 'A' <= ciphertext[i] <= 'Z':
+            key -= ord('A')
         else:
-            key.append(d_small[i])
-    for i in ciphertext:
-        if 'A'<=i<='Z':
-            letters.append(d[i])
-        else:
-            letters.append(d_small[i])
-    #  найдем верную букву по ключу словаря и добавим ее в расшифрованное слово:
-    for i in range(len(letters)):
-        s = (letters[i]-key[i]+26) % 26
-        if 'A'<=ciphertext[1]<='Z':
-            for k, v in d.items():  # k-key; v-value
-                if s == v:
-                    plaintext += k
-        else:
-            for k, v in d_small.items():
-                if s == v:
-                    plaintext += k
+            key -= ord('a')
+        sum = (ord(ciphertext[i]) - key)
+        if sum < ord('A') and 'A' <= ciphertext[i] <= 'Z':
+            sum += 26
+        elif sum < ord('a') and 'a' <= ciphertext[i] <= 'z':
+            sum += 26
+        plaintext += chr(sum)
+    print(plaintext)
     return plaintext
