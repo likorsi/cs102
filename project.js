98078bb6c6975d895939d2555c35caf9de73bdd3
likorsi <43271724+likorsi@users.noreply.github.com>
2018-12-08 02:26:07 +0300

    Update project.js

--- project.js
+++ project.js
@@ -1,61 +1,63 @@
 //нормализация[0,1] рейтинга
-function Normalize_rating(players) {
+function Noramalize_rating(){
 	var max_rating = 5000;
-	var arr_rating = []  //нормализованный массив
-
+	
 	for (var x = 0; x < players.length; x++)
-		arr_rating[x] = players[x][0] / max_rating;
-	return arr_rating;
+		players[x][0] = players[x][0] / max_rating;
+	return players;
 }
 
 
-function Balance(players, class) {
-	normalize_rating();
-
-	var arr_result = []; //итоговый рейтинг игрока
-
-	for (var x = 0; x < arr_rating.length; x++){
-		/*баланс по классу
+function balance() {
+	 Noramalize_rating();
+   /*баланс по классу
 		'dps':0.8, 'tank':0.6, 'support':0.4 */
 		for (var x = 0; x < players.length; x++){
-			if (players[x][1] == 'dps'){
-				arr_result[x] = arr_rating[x] * 0.8;
-			} else if (players[x][1] == 'tank'){
-				arr_result[x] = arr_rating[x] * 0.6;
-			} else if (players[x][1] == 'support'){
-				arr_result[x] = arr_rating[x] * 0.4;
+			if (players[x][1] === 'dps'){
+				arr_result[x] = players[x][0] * 0.8;
+			} else if (players[x][1] === 'tank'){
+				arr_result[x] = players[x][0] * 0.6;
+			} else if (players[x][1] === 'support'){
+				arr_result[x] = players[x][0] * 0.4;
 			}
-
+      //баланс при игре пользователя за одиного персонажа/класс 
+      if (players[x][2] === 1){
+      arr_result[x] *= 0.6;
+      } else if (players[x][3] === 1){
+      arr_result[x] *= 0.9;
+      }   
 		}
-
-		//баланс при игре пользователя за одиного персонажа/класс 
-		if (players[x][2] == True){
-			arr_result[x] *= 0.6;
-		} else if (players[x][3] == True){
-			arr_result[x] *= 0.9;
-		}
-	}
-	
+    
 	return arr_result;
-}
+ }
 
+	
 
-function Sort_rating(arr_result){
+function sort_rating(){
     var n = arr_result.length;
     for (var i = 0; i < n-1; i++)
      { for (var j = 0; j < n-1-i; j++)
         { if (arr_result[j+1] < arr_result[j])
            { var t = arr_result[j+1]; arr_result[j+1] = arr_result[j]; arr_result[j] = t; }
         }
-     }                     
+     }   
+    a = arr_result.reverse();
+    arr_result = a;
+
     return arr_result;
 }
-
 
 /* вложенный массив с критериями игроков из лобби
 структура: [[SR, class, one_class_player, one_character_player]]
 пример: players = [[1500, 'dps', True, False], [4756, 'support', False. False]] */
-var players = [];
+var players = [
+[2000, 'dps', 0, 0], 
+[4500, 'support', 1, 0],
+[2500, 'dps', 0, 0], 
+[3500, 'tank', 1, 1]
+];
+
+var arr_result = []; //итоговый рейтинг игрока
 
 balance();
 sort_rating();
@@ -68,3 +70,5 @@ for (var x = 0; x < arr_result.length; x++){
 	team2.push(arr_result[x+1]);
 	x += 1;
 }
+
+console.log(true)
