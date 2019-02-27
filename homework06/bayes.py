7f30e9fd218f46b06ec6a865171a3a42395d6937
likorsi <li_korsi@mail.ru>
2019-02-27 19:13:12 +0300

    update

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -1,30 +1,35 @@
 import numpy as np
 import string
 
+
 class NaiveBayesClassifier:
 
     def __init__(self, alpha=1):
         self.alpha = alpha
         self.labels = []
         self.table = dict()
-        self.p_labels = []
+        self.p_labels = dict()
 
     def fit(self, titles, y):
 
-        self.labels = [i for i in set(y)] 
+        self.labels = [i for i in y] 
 
         unic_labels = []
+        for i in self.labels:
+            if i not in unic_labels:
+                unic_labels.append(i) 
+
         for i in range(len(self.labels)):
-            if self.labels[i] not in unic_labels:
-                unic_labels.append(self.labels[i]) 
+            if self.p_labels.get(self.labels[i]):
+                a = self.p_labels[self.labels[i]]
+                a += 1 
+                self.p_labels[self.labels[i]] = a
+            else:
+                self.p_labels[self.labels[i]] = 1      
 
-        self.p_labels = [[0] * 2] * len(unic_labels)
-        for i in range(len(self.labels)): 
-            self.p_labels[unic_labels.index(self.labels[i])][0] += 1
-            self.p_labels[unic_labels.index(self.labels[i])][1] = self.labels[i]
-
-        for i in range(len(self.p_labels)):
-            self.p_labels[i][0] /= len(self.labels)
+        for key, value in self.p_labels.items():
+            value /= len(self.labels)
+            self.p_labels[key] = np.log(value)
 
         tr = str.maketrans("", "", string.punctuation)
 
@@ -33,37 +38,39 @@ class NaiveBayesClassifier:
             words = titles[i].split(' ')        
             for word in words:
                 if self.table.get(word):
-                    self.table[word][unic_labels.index(self.labels[i])] += 1
+                    a = self.table[word]
+                    a[unic_labels.index(self.labels[i])] += 1
+                    self.table[word] = a
                 else:
-                    self.table[word] = [0] * len(unic_labels) * 2
-                    self.table[word][unic_labels.index(self.labels[i])] = 1
+                    a = [0 for _ in range(len(unic_labels) * 2)]
+                    a[unic_labels.index(self.labels[i])] = 1
+                    self.table[word] = a
         
         for key, value in self.table.items():
             for i in range(len(unic_labels)):
                 m = self.table.values()
                 words_in_label = 0
-                for j in len(m):
-                    words_in_label += m[j][i] 
-                p = (self.alpha + value[k]) / (self.alpha * len(self.table.keys()) + words_in_label)
+                for j in m:
+                    words_in_label += j[i] 
+                p = (self.alpha + value[i]) / (self.alpha * len(self.table.keys()) + words_in_label)
                 self.table[key][i+len(unic_labels)] = p
         
 
     def predict(self, x):
 
-        result_label = []
-        words = set()
+        result_label = [[] for _ in range(len(x))]
+        tr = str.maketrans("", "", string.punctuation)
         for i in range(len(x)):
             x[i] = x[i].translate(tr).lower()
-            words.add(x[i].split(' '))
-            sum_log = []
-            for i in range(len(self.p_labels)):
-                sum_log.append(self.p_labels[0])
+            words = x[i].split(' ')
+            sum_log = [value for value in self.p_labels.values()]
+
             for word in words:
-                if self.table[word]:
+                if self.table.get(word):
                     for k in range(len(self.p_labels)):
                         sum_log[k] += np.log(self.table[word][k+len(self.p_labels)])
-                    
-            result_label[i] = self.p_labels[sum_log.argmax()][1]
+                
+            result_label[i] = list(self.p_labels)[np.argmax(sum_log)]
 
         return result_label
 
